6cffd2bff79043a5d04b8f4911f1e63cc809c18d
Sencer Erim <sencerim@gmail.com>
2026-06-04 06:49:07 +0300

    chore: export Increment 4E symbol selection helpers

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,31 @@
 """Validated, provenance-aware historical market-data boundary."""
+
+from src.data.symbol_selection import (
+    ExchangeSymbolEvidence,
+    SymbolCandidate,
+    SymbolMarketStats,
+    SymbolSelectionDecision,
+    SymbolSelectionError,
+    SymbolSelectionPolicy,
+    SymbolSelectionReason,
+    SymbolSelectionReport,
+    SymbolSelectionStatus,
+    assert_symbol_selection_has_candidates,
+    harden_symbol_selection,
+    symbol_selection_json,
+)
+
+__all__ = [
+    "ExchangeSymbolEvidence",
+    "SymbolCandidate",
+    "SymbolMarketStats",
+    "SymbolSelectionDecision",
+    "SymbolSelectionError",
+    "SymbolSelectionPolicy",
+    "SymbolSelectionReason",
+    "SymbolSelectionReport",
+    "SymbolSelectionStatus",
+    "assert_symbol_selection_has_candidates",
+    "harden_symbol_selection",
+    "symbol_selection_json",
+]
