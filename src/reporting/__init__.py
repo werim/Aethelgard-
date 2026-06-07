9945a9081fec06a7e3a2afcff94ad7b459ca629d
Sencer Erim <sencerim@gmail.com>
2026-06-07 23:26:36 +0300

    Export Gate 4B-0 performance publication boundary

--- src/reporting/__init__.py
+++ src/reporting/__init__.py
@@ -10,14 +10,24 @@ from src.reporting.paper_db_audit import (
     paper_db_audit_json,
     render_paper_db_audit_markdown,
 )
+from src.reporting.performance_boundary import (
+    MetricPublicationEligibility,
+    MetricPublicationStatus,
+    evaluate_metric_publication_eligibility,
+    metric_publication_eligibility_json,
+)
 
 __all__ = [
+    "MetricPublicationEligibility",
+    "MetricPublicationStatus",
     "PaperDbAuditError",
     "PaperDbAuditIssue",
     "PaperDbAuditReport",
     "PaperDbAuditStatus",
     "assert_paper_db_audit_clean",
     "audit_paper_runtime_database",
+    "evaluate_metric_publication_eligibility",
+    "metric_publication_eligibility_json",
     "paper_db_audit_json",
     "render_paper_db_audit_markdown",
 ]
