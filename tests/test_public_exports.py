77c94073dbb02871121f73684c8213d858a923f8
Sencer Erim <sencerim@gmail.com>
2026-06-11 06:12:49 +0300

    test: fix public export import ordering

--- tests/test_public_exports.py
+++ tests/test_public_exports.py
@@ -1,5 +1,4 @@
-import importlib
-from types import ModuleType
+from importlib import import_module
 
 
 PUBLIC_EXPORT_MODULES = (
@@ -28,13 +27,9 @@ FORBIDDEN_DIRECT_EXPORTS = {
 }
 
 
-def _load_public_module(module_name: str) -> ModuleType:
-    return importlib.import_module(module_name)
-
-
 def test_public_exports_are_declared_attributes() -> None:
     for module_name in PUBLIC_EXPORT_MODULES:
-        module = _load_public_module(module_name)
+        module = import_module(module_name)
 
         for exported_name in module.__all__:
             assert hasattr(module, exported_name), (module_name, exported_name)
@@ -42,7 +37,7 @@ def test_public_exports_are_declared_attributes() -> None:
 
 def test_public_exports_do_not_expose_unsafe_direct_names() -> None:
     for module_name in PUBLIC_EXPORT_MODULES:
-        module = _load_public_module(module_name)
+        module = import_module(module_name)
         normalized_exports = {exported_name.lower() for exported_name in module.__all__}
 
         assert FORBIDDEN_DIRECT_EXPORTS.isdisjoint(normalized_exports), module_name
